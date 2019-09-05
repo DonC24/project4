ceb5ab1fa63ed2f9909ba67b6a4ce141297d768d
DonC24 <51435215+DonC24@users.noreply.github.com>
2019-09-05 09:48:52 +0800

    Added name to users, registration and editing. created users seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create :name => "Jenny", :email => "jenny@abc.com", :password => "pwjenny"
+User.create :name => "David", :email => "david@abc.com", :password => "pwdavid"
+User.create :name => "Will", :email => "will@abc.com", :password => "pwwill"
+User.create :name => "Nicole", :email => "nicole@abc.com", :password => "pwnicole"
+User.create :name => "Brenda", :email => "brenda@abc.com", :password => "pwbrenda"
+User.create :name => "James", :email => "james@abc.com", :password => "pwjames"
+User.create :name => "Alicia", :email => "alicia@abc.com", :password => "pwalicia"
+User.create :name => "Michael", :email => "michael@abc.com", :password => "pwmichael"
+User.create :name => "Kenny", :email => "kenny@abc.com", :password => "pwkenny"
+User.create :name => "Ben", :email => "ben@abc.com", :password => "pwben"
+
+p "USERS HAVE BEEN SEEDED"
