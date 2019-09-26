84e5e57a0127b937bc6181f48fb147e46194a2ee
DonC24 <51435215+DonC24@users.noreply.github.com>
2019-09-26 17:14:26 +0800

    created a map function for events

--- app/javascript/components/dashboard.jsx
+++ app/javascript/components/dashboard.jsx
@@ -8,85 +8,60 @@ class Dashboard extends React.Component {
         super(props);
     }
 
-  render() {
-    //console.log("in dashboard: ");
-    let allusers = this.props.allusers;
-    let currentuser = this.props.currentuser;
-    let upcomingevents = this.props.upcomingevents;
-    let matched = this.props.matchedperson;
+    mapfunc(array) {
+        return array.map(anevent => { //need return here to return the entire output of the map
+            // console.log(anevent);
+            // console.log("matched");
+            // console.log(matched);
+            let matched = this.props.matchedperson;
+            let currentuser = this.props.currentuser;
+            let matchpersonevent = matched.find(amatch => amatch.event_id === anevent.id);
+            // console.log(matchpersonevent)
+            let allusers = this.props.allusers;
+            let person = allusers.find(aperson => aperson.id === matchpersonevent.recipient_id);
+            // console.log(person);
+            let formatteddate = Moment(anevent.eventdate).utc().format("dddd, DD MMM YY, h:mm a");
+            let seematched = "";
+            let thiseventid = anevent.id;
+            // console.log(thiseventid);
 
-    let upcominglist = upcomingevents.map(anevent => {
-        // console.log(anevent);
-        // console.log("matched");
-        // console.log(matched);
-        let matchpersonevent = matched.find(amatch => amatch.event_id === anevent.id);
-        // console.log(matchpersonevent)
-        let person = allusers.find(aperson => aperson.id === matchpersonevent.recipient_id);
-        // console.log(person);
-        let formatteddate = Moment(anevent.eventdate).utc().format("dddd, DD MMM YY, h:mm a");
-        let seematched = "";
-        let thiseventid = anevent.id;
-        console.log(thiseventid);
+            if(currentuser === anevent.user_id){
+                seematched = <button className="btn btn-outline-info btn-sm" value={`${thiseventid}`} onClick={(event)=>{this.props.allMatchesClick(event)}}>See list of matched persons</button>
+            }
+            return( //this only returns once per iteration of the map loop
 
-        if(currentuser === anevent.user_id){
-            seematched = <button className="btn btn-outline-info btn-sm" value={`${thiseventid}`} onClick={(event)=>{this.props.allMatchesClick(event)}}>See list of matched persons</button>
-        }
-
-        return(
-            <div className="col-xs-12 col-sm-6 col-md-6 col-lg-4 cards">
-                <div className="card" style={{width: 18 + "rem"}}>
-                    <div className="card-body">
-                        <h5 className="card-title">{anevent.name}</h5>
-                        <h6 className="card-subtitle mb-2 text-muted">{formatteddate}</h6>
-                        <p className="card-text">Notes about this event: <br />
-                        {anevent.notes}</p>
-                        <p className="card-text">The recipient of your gift is: <br />
-                        {person.name} ({person.email})</p>
-                        <p className="card-text">{seematched}</p>
-                    </div>
-                    <div className="card-footer">
-                        <button className="btn btn-primary" value={`${thiseventid}`} onClick={(event)=>{this.props.handleDetailsClick(event)}}>Event Details</button>
+                <div className="col-xs-12 col-sm-6 col-md-6 col-lg-4 cards">
+                    <div className="card" style={{width: 18 + "rem"}}>
+                        <div className="card-body">
+                            <h5 className="card-title">{anevent.name}</h5>
+                            <h6 className="card-subtitle mb-2 text-muted">{formatteddate}</h6>
+                            <p className="card-text">Notes about this event: <br />
+                            {anevent.notes}</p>
+                            <p className="card-text">The recipient of your gift is: <br />
+                            {person.name} ({person.email})</p>
+                            <p className="card-text">{seematched}</p>
+                        </div>
+                        <div className="card-footer">
+                            <button className="btn btn-primary" value={`${thiseventid}`} onClick={(event)=>{this.props.handleDetailsClick(event)}}>Event Details</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        )
-    })
+            )
+        })
+    }
+
+
+  render() {
+    console.log("in dashboard: ");
+
+
+    let upcomingevents = this.props.upcomingevents;
 
     let pastevents = this.props.pastevents;
-    let pastlist = pastevents.map(anevent => {
-        //console.log(anevent.eventdate);
-        let matchpersonevent = matched.find(amatch => amatch.event_id === anevent.id);
-        // console.log(matchpersonevent)
-        let person = allusers.find(aperson => aperson.id === matchpersonevent.recipient_id);
-        // console.log(person);
-        let formatteddate = Moment(anevent.eventdate).utc().format("dddd, DD MMM YY, h:mm a");
-        let seematched = "";
 
-        let thiseventid = anevent.id;
-        // console.log(thiseventid);
+    let upcominglist = this.mapfunc(upcomingevents);
+    let pastlist = this.mapfunc(pastevents);
 
-        if(currentuser === anevent.user_id){
-            seematched = <button className="btn btn-outline-info btn-sm" value={`${thiseventid}`} onClick={(event)=>{this.props.allMatchesClick(event)}}>See list of matched persons</button>
-        }
-        return(
-            <div className="col-xs-12 col-sm-6 col-md-6 col-lg-4 cards">
-                <div className="card" style={{width: 18 + "rem"}}>
-                    <div className="card-body">
-                        <h5 className="card-title">{anevent.name}</h5>
-                        <h6 className="card-subtitle mb-2 text-muted">{formatteddate}</h6>
-                        <p className="card-text">Notes about this event: <br />
-                        {anevent.notes}</p>
-                        <p className="card-text">The recipient of your gift is: <br />
-                        {person.name} ({person.email})</p>
-                        <p className="card-text">{seematched}</p>
-                    </div>
-                    <div className="card-footer">
-                        <button className="btn btn-primary" value={`${thiseventid}`} onClick={(event)=>{this.props.handleDetailsClick(event)}}>Event Details</button>
-                    </div>
-                </div>
-            </div>
-        )
-    })
 
     return (
         <div>
